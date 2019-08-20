598d30f3c35b0c7a679f873ce259eaaea78f61ff
reza aulia <zenhuwmank@gmail.com>
2019-08-20 18:00:08 +0700

    first commit

---
+++ application/views/main/footer.php
@@ -0,0 +1,18 @@
+      <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 2.0.7
+        </div>
+        <strong>Created by <a href="mailto:muh.hatta@me.com">Muh. Hatta </a> &copy; 2016</strong>
+      </footer>
+
+      
+      
+    </div><!-- ./wrapper -->
+
+<?php
+	include('edit_profile_js.php');
+	include('edit_profile_form.php');
+?>	
+</body>
+  
+</html>
